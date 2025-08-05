39c47f7bb9526b51846e5b3ed50fa64fb67524e9
mywpbot18 <mywpbot18@gmail.com>
2025-08-05 22:44:19 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~wqVQlJBS#sou_3bJmIf9woOCFa8xMuByEN4Kwb9FeZV-zkync8H4",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
